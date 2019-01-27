69221a4084537f191637ffd914afac88b1881866
Sarvesh Sawant <sarvesh.sawant@wonderbiz.in>
2019-01-27 20:48:53 +0530

    first Commit

---
+++ DatingSkeleton/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using DatingSkeleton.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingSkeleton.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
